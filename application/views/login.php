1e7303e86ff25f05717d2ba1a395286b4719a170
Kevin Fenger <kevindfenger@gmail.com>
2014-05-02 12:27:36 -0500

    checking in views and assets

---
+++ application/views/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+<body>
+
+<div id="container">
+<a href='#' id='fb_login'><img src='assets/img/fb_login.png'></img></a>
+</div>
+
+</body>
+</html>
